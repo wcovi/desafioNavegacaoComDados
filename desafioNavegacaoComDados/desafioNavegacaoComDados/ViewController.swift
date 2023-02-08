e310f99c815b531e179ec3dd9c296d401088c6cb
wcovi <wcovi@yahoo.com.br>
2023-02-07 21:45:59 -0300

    feature: project commit

---
+++ desafioNavegacaoComDados/desafioNavegacaoComDados/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  desafioNavegacaoComDados
+//
+//  Created by Wolfgang Florian Covi on 07/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
